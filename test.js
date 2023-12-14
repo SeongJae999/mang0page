826eb1c70f86937efdb20a3b84575d2ef02217fe
skanehfud279 <skanehfud279@gmail.com>
2023-12-14 14:41:49 +0900

    add js file

---
+++ test.js
@@ -0,0 +1,6 @@
+function check(form) {
+    if (form.userid.value=="skanehfud279" && form.userpassword.value=="tmvkdlzm279@")
+        window.open('target.html')
+    else
+        alert("Error UserID or Password");
+}
