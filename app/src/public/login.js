b4d80ac3ca33edd1f111cfd46b7165c82ec48210
skanehfud279 <skanehfud279@gmail.com>
2023-12-27 22:09:53 +0900

    add nodemon

---
+++ app/src/public/login.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const id = documnet.querySelector("#id"),
+password = document.querySelector("#password"),
+loginBtn = documnet.querySelector("#button");
+
+console.log(id);
+console.log("hello");
