af07714bb34416b0c6f38de4e94953e4dbe714b2
skanehfud279 <skanehfud279@gmail.com>
2023-12-29 15:32:18 +0900

    dom use login data save

--- app/src/public/js/home/test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-function check(form) {
-    if (form.userid.value=="skanehfud279" && form.userpassword.value=="tmvkdlzm279@")
-        window.open('target.html');
-    else
-        alert("Error UserID or Password");
-}
--- app/src/public/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const id = documnet.querySelector("#id"),
-password = document.querySelector("#password"),
-loginBtn = documnet.querySelector("#button");
-
-console.log(id);
-console.log("hello");
